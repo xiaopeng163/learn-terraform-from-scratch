5cbbeb12836dea144950b89e1242aa74e0f5dec3
Peng Xiao <xiaoquwl@gmail.com>
2022-08-22 23:51:51 +0200

    update

--- code/function-loops/main.tf
+++ code/function-loops/main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
 locals {
   # Common tags to be assigned to all resources
   common_tags = {
@@ -22,12 +11,6 @@ output "vpc_id" {
 
   value = aws_vpc.vpc[*].id
 
-}
-
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.aws_region
 }
 
 # use count
@@ -44,31 +27,4 @@ resource "aws_vpc" "my_vpc" {
   cidr_block           = "192.168.0.0/16"
   enable_dns_hostnames = var.enable_dns_hostnames
   tags                 = local.common_tags
-}
-
-# create security group
-resource "aws_security_group" "my_sg" {
-  name   = "HTTP and SSH"
-  vpc_id = aws_vpc.my_vpc.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
--- source/function_looping.rst
+++ source/function_looping.rst
@@ -1,2 +1,12 @@
 Functions and Looping
 =======================
+
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   function_loops/count
+   function_loops/for-each
+   function_loops/function
