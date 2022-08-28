e88028876cb5428ae6fbb27a5d7939c012e1d676
Peng Xiao <xiaoquwl@gmail.com>
2022-08-29 00:32:50 +0200

    update

--- code/module/network.tf
+++ code/module/network.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.14.2"
+  version = ">= 3.14.2"
 
   name           = var.aws_vpc_name
   cidr           = var.aws_vpc_cidr
