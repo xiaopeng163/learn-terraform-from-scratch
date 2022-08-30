f678bc7e07f48f17bc56d9952877857e539220ad
Peng Xiao <xiaoquwl@gmail.com>
2022-08-30 13:31:27 +0200

    update

---
+++ code/manage-state/s3/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "kops.k8s.imooc.link"
+    key            = "test/ecs-platform"
+    region         = "us-west-1"
+  }
+}
