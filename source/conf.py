50ebc060edac993dc61b65405ac285a96201df4f
Peng Xiao <xiaoquwl@gmail.com>
2022-09-21 10:00:42 +0000

    update

--- source/conf.py
+++ source/conf.py
@@ -32,7 +32,7 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinxemoji.sphinxemoji", "sphinx_contributors"
+    "sphinxemoji.sphinxemoji", "sphinx_contributors", "sphinx_copybutton"
 ]
 sphinxemoji_style = 'twemoji'
 
@@ -53,7 +53,12 @@ exclude_patterns = []
 #
 #html_theme = 'sphinx_rtd_theme'
 html_theme = "furo"
-
+html_title = "Learn Terraform from Scratch"
+html_theme_options = {}
+html_theme_options["announcement"] = (
+        "🙏🙏🙏 如果大家发现文章中的错误，欢迎提PR或者issue指正 "
+        "<a href='https://github.com/xiaopeng163/learn-terraform-from-scratch/issues/new'>文档源码地址</a>."
+)
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
