154e7f2bd5ce10e0a2e1f188b9495f40ba79a672
Peng Xiao <xiaoquwl@gmail.com>
2022-09-24 00:25:26 +0200

    update

--- code/manage-state/import/network.tf
+++ code/manage-state/import/network.tf
@@ -1,9 +1,9 @@
 # create vpc
 resource "aws_vpc" "my-vpc" {
 
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
+  cidr_block           = "10.0.0.0/24"
+  enable_dns_hostnames = false
   tags                 = {
-    Name = "my-vpc"
+    Name = "my-vpc-1"
   }
 }
